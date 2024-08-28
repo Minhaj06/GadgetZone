1a92afc476a7184a016f7f58746337c31186388a
Minhaj06 <milonmdminhajkobir@gmail.com>
2024-08-28 23:14:14 +0600

    Working on add product form

--- GadgetZoneClient/src/pages/admin/product/Product.jsx
+++ GadgetZoneClient/src/pages/admin/product/Product.jsx
@@ -1,14 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Upload, Modal, Button, Input, Form, Select, Row, Col } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
+import { useAuth } from "../../../context/auth";
+import axios from "axios";
 
 const { Option } = Select;
 
 const ProductUpload = () => {
+  const { setIsLoading } = useAuth();
+
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
+  const [filteredSubcategories, setFilteredSubcategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const [form] = Form.useForm();
   const [photoList, setPhotoList] = useState([]);
   const [previewVisible, setPreviewVisible] = useState(false);
   const [previewImage, setPreviewImage] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      try {
+        const { data } = await axios.get(`/categories`);
+        setCategories(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+    (async () => {
+      setIsLoading(true);
+      try {
+        const { data } = await axios.get(`/subcategories`);
+        setSubcategories(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
 
   const handlePreview = async (file) => {
     setPreviewImage(file.url || file.thumbUrl);
@@ -26,16 +59,27 @@ const ProductUpload = () => {
     setPhotoList(newPhotoList);
   };
 
+  const handleCategoryChange = (categoryId) => {
+    console.log("categoryId", categoryId);
+    setSelectedCategory(categoryId);
+    console.log("subcategories", subcategories);
+    const filtered = subcategories.filter(
+      (subcategory) => subcategory?.category?._id === categoryId
+    );
+    console.log("filtered", filtered);
+    setFilteredSubcategories(filtered);
+    form.setFieldsValue({ subcategory: null });
+  };
+
   const onFinish = (values) => {
-    // Handle form submission logic here
     console.log("Submitted values:", values);
     console.log("Selected images:", photoList);
   };
 
-  const initialValues = {
-    name: "BenQ Monitor",
-    description: "Best quality BenQ Monitor",
-  };
+  // const initialValues = {
+  //   name: "BenQ Monitor",
+  //   description: "Best quality BenQ Monitor",
+  // };
 
   const uploadButton = (
     <button
@@ -62,7 +106,7 @@ const ProductUpload = () => {
         form={form}
         onFinish={onFinish}
         layout="vertical"
-        initialValues={initialValues}
+        // initialValues={initialValues}
         labelCol={{ style: { fontWeight: 600 } }}
       >
         {/*  */}
@@ -100,7 +144,7 @@ const ProductUpload = () => {
                   name="name"
                   rules={[{ required: true, message: "Please enter the product name" }]}
                 >
-                  <Input size="large" value={initialValues.name} />
+                  <Input size="large" />
                 </Form.Item>
               </Col>
               <Col xs={24}>
@@ -121,7 +165,7 @@ const ProductUpload = () => {
               name="description"
               rules={[{ required: true, message: "Please enter the product description" }]}
             >
-              <Input.TextArea size="large" value={initialValues.description} rows={5} />
+              <Input.TextArea size="large" rows={5} />
             </Form.Item>
           </Col>
 
@@ -131,9 +175,22 @@ const ProductUpload = () => {
               name="category"
               rules={[{ required: true, message: "Please select the product category" }]}
             >
-              <Select size="large">
-                <Option value="640cb019e2276b6fdc75223d">Electronics</Option>
-              </Select>
+              <Select
+                size="large"
+                showSearch
+                options={categories.map((category) => ({
+                  value: category?._id,
+                  label: category?.name,
+                }))}
+                optionFilterProp="label"
+                filterSort={(optionA, optionB) =>
+                  (optionA?.label ?? "")
+                    .toLowerCase()
+                    .localeCompare((optionB?.label ?? "").toLowerCase())
+                }
+                placeholder="Select Category"
+                onChange={handleCategoryChange}
+              ></Select>
             </Form.Item>
           </Col>
           <Col xs={24} lg={12}>
@@ -142,9 +199,22 @@ const ProductUpload = () => {
               name="subcategory"
               rules={[{ required: true, message: "Please select the product category" }]}
             >
-              <Select size="large">
-                <Option value="640de10ca62831581e493cfe">Monitor</Option>
-              </Select>
+              <Select
+                size="large"
+                showSearch
+                options={filteredSubcategories.map((subcategory) => ({
+                  value: subcategory?._id,
+                  label: subcategory?.name,
+                }))}
+                optionFilterProp="label"
+                filterSort={(optionA, optionB) =>
+                  (optionA?.label ?? "")
+                    .toLowerCase()
+                    .localeCompare((optionB?.label ?? "").toLowerCase())
+                }
+                placeholder="Select Subcategory"
+                disabled={!selectedCategory}
+              ></Select>
             </Form.Item>
           </Col>
           <Col xs={24} lg={12}>
